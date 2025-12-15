9d6b3f5c25ca53afaf17edcf3e28f9a3cf8bae69
Lucky <luckyhou168@gmail.com>
2025-12-15 18:46:29 -0500

    Add Terraform infrastructure baseline

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.55.0"
+    }
+  }
+
+  required_version = ">= 1.5.7, <2.0.0"
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,14 @@
+# Sets global variables for this Terraform project.
+
+
+variable app_name {
+  default = "bootmicro"
+}
+
+variable location {
+  default = "eastus"
+}
+
+variable kubernetes_version {    
+  default = "1.34.1"
+}
